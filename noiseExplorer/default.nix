3b372b8ed0b620f19ff00ff88112a4148c14dc1e
fuzzy0 <raghurammailbox@gmail.com>
2021-09-07 05:04:45 +0000

    Noise Explorer command-line tool  (#1)
    * modified
    
    * added README
    
    * format check implemented and remarks in review addressed
    
    * q
    
    * default.nix and shell.nix restored as per the template

---
+++ noiseExplorer/default.nix
@@ -0,0 +1,38 @@
+{ stdenv, lib, fetchgit, fetchurl, nodejs, pkgs }:
+let
+  nodeDependencies = (pkgs.callPackage ./dep.nix { }).shell.nodeDependencies;
+in
+
+stdenv.mkDerivation rec {
+  pname = "NoiseExplorer";
+
+  version = "1.0.3";
+
+  src = fetchgit {
+    url = "https://source.symbolic.software/noiseexplorer/noiseexplorer.git";
+    rev = "5b03267416fd5deb8b08f9d254b4c64b00baa676";
+    sha256 = "sha256-hXTKrRDpdmHvGgIyPnyjYopNgvewvxbzEgBxOfFa62w=";
+  };
+
+  buildInputs = with pkgs; [ cargo nodejs go rustc wasm wasm-pack ];
+
+  buildPhase = ''
+    ln -s ${nodeDependencies}/lib/node_modules ./node_modules
+    export PATH="${nodeDependencies}/bin:$PATH"
+    cd src
+    pegjs -o parser/noiseParser.js parser/noiseParser.pegjs
+    echo -n "[NoiseExplorer] Generating NoiseParser..."
+    echo "Parser Generated"
+    cd util
+    bash genModels.sh
+    bash genHtml.sh
+  '';
+
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cd ../../
+    cp -vr . $out
+    cp ${pkgs.writeScript "noiseexplorer" ''${nodejs}/bin/node ${src}/src/noiseExplorer.js $0 $1 $2''} $out/bin/noiseexplorer
+  '';
+}
